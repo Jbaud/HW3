d1002b2846f076376d497c09c260d2dfef27b5f5
Jules Baud <baud.jules@gmail.com>
2016-10-23 17:54:01 -0500

    kind of working

--- copie.c
+++ copie.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <mpi.h>
+#include <time.h>
 
 #define ROW 4
 #define COLUMN 4
@@ -15,42 +16,42 @@ double **alloc_2d_double(int rows, int cols) {
 }
 
 int **alloc_2d_int(int rows, int cols) {
-    int *data = (int *)malloc(rows*cols*sizeof(int));
-    int **array= (int **)malloc(rows*sizeof(int*));
-    for (int i=0; i<rows; i++)
-        array[i] = &(data[cols*i]);
+	int *data = (int *)malloc(rows*cols*sizeof(int));
+	int **array= (int **)malloc(rows*sizeof(int*));
+	for (int i=0; i<rows; i++)
+		array[i] = &(data[cols*i]);
 
-    return array;
+	return array;
 }
 
 void printArray_int(int rows, int cols, int array[rows][cols]){
 
-                            int i,j;
+	int i,j;
 
-                            for(i = 0; i < rows; i++)
-                            {
-                            for(j = 0; j < cols; j++) 
-                            {
-                            array[i][j]=0;
-                            printf("%d ", array[i][j]); 
-                            }
-                            printf("\n");
-                           }
+	for(i = 0; i < rows; i++)
+	{
+		for(j = 0; j < cols; j++) 
+		{
+			array[i][j]=0;
+			printf("%d ", array[i][j]); 
+		}
+		printf("\n");
+	}
 }
 
 void printArray_double(int rows, int cols, double array[rows][cols]){
 
-                            int i,j;
+	int i,j;
 
-                            for(i = 0; i < rows; i++)
-                            {
-                            for(j = 0; j < cols; j++) 
-                            {
-                            array[i][j]=0;
-                            printf("%lf ", array[i][j]); 
-                            }
-                            printf("\n");
-                           }
+	for(i = 0; i < rows; i++)
+	{
+		for(j = 0; j < cols; j++) 
+		{
+			array[i][j]=0;
+			printf("%lf ", array[i][j]); 
+		}
+		printf("\n");
+	}
 }
 
 int main(int argc, char **argv)
@@ -59,8 +60,6 @@ int main(int argc, char **argv)
 	int size;
 	int rank;
 	MPI_Status status;
-	int source = 0;
-	int tag = 0;
 
 	/*Start MPI*/
 	MPI_Init(&argc, &argv);
@@ -71,29 +70,26 @@ int main(int argc, char **argv)
 	/*Get the total number of processes*/
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	if(rank == source)
+	if(rank == 0)
 	{
 		double **mat;
 		double *vector;
-		int *columns_to_send;
-		int k = 0;
 		int i;
 		int j;
 		int base;
-		int remainder;
 		int nb_columns;
-		int columns_length;
-
-                            /*
-		mat = malloc(sizeof(double*)*(ROW*COLUMN));
 
 
-		for(i = 0; i < (ROW*COLUMN); i++)
+		mat = alloc_2d_double(ROW,COLUMN);
+		vector = malloc(sizeof(double)*ROW);
+		//random vector to make input easier
+		srand((time(NULL)));
+
+		for(i = 0; i < ROW; i++)
 		{
-			mat[i] = malloc(sizeof(double)*(ROW*COLUMN));
+			vector[i] = ((rand()%(9))+1)/10.0;  
 		}
-                            */
-                          mat = alloc_2d_double(ROW,COLUMN);
+
 
 		FILE *file;
 		file=fopen("test4_4.txt", "r");
@@ -114,70 +110,83 @@ int main(int argc, char **argv)
 
 
 		// sending infos to other processes
-		columns_to_send = malloc(sizeof(int)*size);
 
 		base= ROW/size;
-		columns_length=ROW*COLUMN;
+		int k =  0;
 
-		for(i = 0; i < size; i++)
+		int **toCompute;
+		toCompute = alloc_2d_int(size,base);
+
+		for (i = 0; i < size; ++i)
 		{
-			columns_to_send[i] = base;
-		}
-
-		//k = k + columns_to_send[0];
-                           k =  0;
-
-                            int **toCompute;
-                            toCompute = alloc_2d_int(size,base);
-                
-                            for (i = 0; i < size; ++i)
-                            {
-                                for ( j = 0; j < base; ++j)
-                                {
-                                  toCompute[i][j]= k;   
-                                  k++;
-                                }
-
-                            }
-                            printf("\n");
-                            
-                                for(i = 0; i < size; i++)
-                                {
-                                    for(j = 0; j < base; j++) 
-                                    {
-                                        printf("%.d ",toCompute[i][j]); 
-                                    }
-                                    printf("\n");
-                                }
-                            
-                                for (i = 1; i < size; ++i)
-                                {
-                                    MPI_Send(&i, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-                                    MPI_Send(&base, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
-                                    MPI_Send(&(mat[0][0]), size*base, MPI_INT, i, 0, MPI_COMM_WORLD);
-                                }
-
-                          /*
-		for(i = 1; i < size; i++)
-		{
-			nb_columns = columns_to_send[i];
-                                          printf("This is COLUMN : %d and nb_columns: %d\n",i,columns_to_send[i] );
-			MPI_Send(&nb_columns, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
-			MPI_Send(&columns_length, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
-
-			for(j = 0; j < columns_to_send[i]; j++)
+			for ( j = 0; j < base; ++j)
 			{
-				MPI_Send(&mat[k][0], ROW*COLUMN, MPI_DOUBLE, i, tag, MPI_COMM_WORLD);
+				toCompute[i][j]= k;   
 				k++;
 			}
 
 		}
-                           */
+		printf("\n");
+
+		for(i = 0; i < size; i++)
+		{
+			for(j = 0; j < base; j++) 
+			{
+				printf("%.d ",toCompute[i][j]); 
+			}
+			printf("\n");
+		}
+
+		for (i = 1; i < size; ++i)
+		{
+			MPI_Send(&i, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&base, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&(mat[0][0]), size*base, MPI_INT, i, 0, MPI_COMM_WORLD);
+		}
+
 		for (i = 1; i < size; ++i)
 		{
 			MPI_Send(&(mat[0][0]), ROW*COLUMN, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);        
 		}
+		// now we send the vector
+		for (i = 1; i < size; ++i)
+		{
+			MPI_Send(&(vector[0]), ROW, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);        
+		}
 
+		double c =0;
+
+
+                          printf("rank : %d , j=0, j<%d\n",rank,base );
+		// we use a similar algorithm to the sequential code
+		for(j = 0 ; j < base; j++)
+		{
+			for(i = 0; i<ROW; i++)
+			{
+				if(i > j)
+				{
+					c = mat[i][j]/mat[j][j];
+
+					for(k = 0; k<ROW; k++)
+					{
+
+						mat[i][k] = mat[i][k] - c*mat[j][k];
+					}
+					vector[i] = vector[i] - c*vector[j];
+				}
+			}
+		}
+
+
+		printf("this is the output for rank 0:\n");
+		for(i = 0; i < ROW; i++)
+		{
+			for(j = 0; j < COLUMN; j++) 
+			{
+				printf("%.1lf ",mat[i][j]); 
+			}
+			printf("\n");
+		}
 
 	}
 
@@ -186,24 +195,29 @@ int main(int argc, char **argv)
 		int i;
 		int j;
 		int nb_columns;
-		int columns_length;
 		double **local_columns;
-                           int base_copie;
-                           int my_work;
-
-                            MPI_Recv(&my_work, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-                            MPI_Recv(&base_copie, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		double *local_vector;
+		int base_copie;
+		int my_work;
 
 
-                            int **toCompute_copie;
-                            toCompute_copie = alloc_2d_int(size,base_copie);
+		MPI_Recv(&my_work, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&base_copie, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
 
-                           MPI_Recv(&(toCompute_copie[0][0]), size*base_copie, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-                         
+		int **toCompute_copie;
+		toCompute_copie = alloc_2d_int(size,base_copie);
+
+		local_vector  = malloc(sizeof(double)*ROW);
+
+
+		MPI_Recv(&(toCompute_copie[0][0]), size*base_copie, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
 		double **A_copie = alloc_2d_double(ROW,COLUMN);
 
 		MPI_Recv(&(A_copie[0][0]), ROW*COLUMN, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+
+		MPI_Recv(&(local_vector[0]), COLUMN, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
 
 		for (i = 0; i < ROW; ++i)
 		{
@@ -212,6 +226,42 @@ int main(int argc, char **argv)
 				printf("%.1lf ",A_copie[i][j]); 
 			}
 			printf("\n");
+		}
+
+		// algorithm in //
+		double c = 0;
+		int k;
+		printf("i : %d , my_work*base_copie=%d, (my_work*base_copie)+base_copie=%d\n",my_work,my_work*base_copie,(my_work*base_copie)+base_copie );
+		for(j = my_work*base_copie ; j < ((my_work*base_copie)+base_copie); j++)
+		{
+			for(i = 0; i<ROW; i++)
+			{
+				if(i > j)
+				{
+					c = A_copie[i][j]/A_copie[j][j];
+
+					for(k = 1; k<ROW; k++)
+					{
+
+						A_copie[i][k] = A_copie[i][k] - c*A_copie[j][k];
+					}
+
+					local_vector[i] = local_vector[i] - (c*local_vector[j]);
+				}
+			}
+		}
+		// The computation is finished we send back our response
+		if (rank==1)
+		{
+			printf("this is the output for rank 1:\n");
+			for(i = 0; i < ROW; i++)
+			{
+				for(j = 0; j < COLUMN; j++) 
+				{
+					printf("%.1lf ",A_copie[i][j]); 
+				}
+				printf("\n");
+			}
 		}
 
 	}
